57538a4433a9150b3ce6e574acdc1160806bb31e
hvoister <l1u0k607@spoonmob.anonaddy.me>
2024-10-22 12:20:34 +0300

    Logging

--- TestTaskEM/Program.cs
+++ TestTaskEM/Program.cs
@@ -16,6 +16,7 @@ namespace TestTaskEM
             if (File.Exists("test.txt"))
             {
                 string filePath = "test.txt";
+                string logFilePath = "logs.txt";
                 string district;
                 do
                 {
@@ -28,26 +29,31 @@ namespace TestTaskEM
                 string inputTime;
                 do
                 {
-                    Console.Write("Print a delivery time (YYYY-MM-DD HH:MM:SS): ");
+                    Console.Write("Print the time of the first delivery (YYYY-MM-DD HH:MM:SS): ");
                     inputTime = Console.ReadLine();
                 }
                 while (!DateTime.TryParseExact(inputTime, "yyyy-MM-dd HH:mm:ss", null, DateTimeStyles.None, out firstDeliveryTime));
 
 
                 var orders = GetOrders(filePath);
-                var filtredOrders = OrdersFiltration(orders, district, firstDeliveryTime);
+                Logging($"Loaded {orders.Count} orders.", logFilePath);
+                var filteredOrders = OrdersFiltration(orders, district, firstDeliveryTime);
+                Logging($"Found {filteredOrders.Count} orders for {district}.", logFilePath);
 
                 Console.WriteLine();
-                if (filtredOrders.Count > 0)
+                if (filteredOrders.Count > 0)
                 {
-                    foreach (var line in filtredOrders)
+                    foreach (var line in filteredOrders)
                     {
                         Console.WriteLine($"ID: {line.OrderID}\tWeight: {line.Weight}\tDistrict: {line.CityDistrict}\tTime: {line.DeliveryTime}\n");
+                        File.AppendAllText($"test_{district}.txt", $"ID: {line.OrderID}\tWeight: {line.Weight}\tDistrict: {line.CityDistrict}\tTime: {line.DeliveryTime}\n");
                     }
+                    Console.WriteLine("Created a result file and a log file in the project folder.\n");
                 }
                 else Console.WriteLine("There is no data to output.");
             }
-            else Console.WriteLine("Add the data file to the project folder.");
+            else Console.WriteLine("Add the data file (test.txt) to the project folder.");
+            Console.ReadKey();
         }
 
 
@@ -81,6 +87,11 @@ namespace TestTaskEM
             DateTime filtredTime = firstDeliveryTime.AddMinutes(30);
 
             return orders.Where(o => o.CityDistrict == district && (filtredTime >= o.DeliveryTime && o.DeliveryTime >= firstDeliveryTime)).ToList();
+        }
+
+        public static void Logging(string message, string logFilePath)
+        {
+            File.AppendAllText(logFilePath, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}: {message}\n");
         }
     }
 }
---
+++ TestTaskEM/WorkingWithOrders/Logs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskEM.WorkingWithOrders
+{
+    public class Logs
+    {
+        public string IpAddress { get; set; }
+        public DateTime AccessTime { get; set; }
+    }
+}
