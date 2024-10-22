4b9ac1997a96c4c54f84a62d775be857da72aa20
hvoister <l1u0k607@spoonmob.anonaddy.me>
2024-10-22 12:00:41 +0300

    Output and filtering checks

--- TestTaskEM/Program.cs
+++ TestTaskEM/Program.cs
@@ -13,27 +13,41 @@ namespace TestTaskEM
     {
         static void Main(string[] args)
         {
-            string filePath = "test.txt";
-            Console.Write("Print a district: ");
-            string district = Convert.ToString(Console.ReadLine());
-            DateTime firstDeliveryTime;
-            string inputTime;
-            do
+            if (File.Exists("test.txt"))
             {
-                Console.Write("Print a delivery time (YYYY-MM-DD HH:MM:SS): ");
-                inputTime = Console.ReadLine();
+                string filePath = "test.txt";
+                string district;
+                do
+                {
+                    Console.Write("Print a district: ");
+                    district = Convert.ToString(Console.ReadLine());
+                }
+                while (String.IsNullOrEmpty(district));
+
+                DateTime firstDeliveryTime;
+                string inputTime;
+                do
+                {
+                    Console.Write("Print a delivery time (YYYY-MM-DD HH:MM:SS): ");
+                    inputTime = Console.ReadLine();
+                }
+                while (!DateTime.TryParseExact(inputTime, "yyyy-MM-dd HH:mm:ss", null, DateTimeStyles.None, out firstDeliveryTime));
+
+
+                var orders = GetOrders(filePath);
+                var filtredOrders = OrdersFiltration(orders, district, firstDeliveryTime);
+
+                Console.WriteLine();
+                if (filtredOrders.Count > 0)
+                {
+                    foreach (var line in filtredOrders)
+                    {
+                        Console.WriteLine($"ID: {line.OrderID}\tWeight: {line.Weight}\tDistrict: {line.CityDistrict}\tTime: {line.DeliveryTime}\n");
+                    }
+                }
+                else Console.WriteLine("There is no data to output.");
             }
-            while (!DateTime.TryParseExact(inputTime, "yyyy-MM-dd HH:mm:ss", null, DateTimeStyles.None, out firstDeliveryTime));
-
-
-            var orders = GetOrders(filePath);
-            var filtredOrders = OrdersFiltration(orders, district, firstDeliveryTime);
-
-            Console.WriteLine();
-            foreach (var line in orders)
-            {
-                Console.WriteLine($"ID: {line.OrderID}\tWeight: {line.Weight}\tDistrict: {line.CityDistrict}\tTime: {line.DeliveryTime}\n");
-            }
+            else Console.WriteLine("Add the data file to the project folder.");
         }
 
 
@@ -66,8 +80,7 @@ namespace TestTaskEM
         {
             DateTime filtredTime = firstDeliveryTime.AddMinutes(30);
 
-            return orders.Where(o => o.CityDistrict == district && o.DeliveryTime <= filtredTime).ToList();
-
+            return orders.Where(o => o.CityDistrict == district && (filtredTime >= o.DeliveryTime && o.DeliveryTime >= firstDeliveryTime)).ToList();
         }
     }
 }
