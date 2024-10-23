36af71765f923d3cc38329d4d3b258eb6bff0bd4
hvoister <l1u0k607@spoonmob.anonaddy.me>
2024-10-23 16:30:50 +0300

    Version 2

--- TestTaskEM/Program.cs
+++ TestTaskEM/Program.cs
@@ -21,8 +21,9 @@ namespace TestTaskEM
                 string district;
                 do
                 {
-                    Console.Write("Print a district: ");
-                    district = Convert.ToString(Console.ReadLine());
+                    Console.Write("Type a district: ");
+                    district = Console.ReadLine().ToLower();
+                    district = char.ToUpper(district[0]) + district.Substring(1);
                 }
                 while (String.IsNullOrEmpty(district));
 
@@ -30,15 +31,15 @@ namespace TestTaskEM
                 string inputTime;
                 do
                 {
-                    Console.Write("Print the time of the first delivery (YYYY-MM-DD HH:MM:SS): ");
+                    Console.Write("Type the time of the first delivery (YYYY-MM-DD HH:MM:SS): ");
                     inputTime = Console.ReadLine();
                 }
                 while (!DateTime.TryParseExact(inputTime, "yyyy-MM-dd HH:mm:ss", null, DateTimeStyles.None, out firstDeliveryTime));
 
                 var orders = GetOrders(filePath);
-                Logging($"Loaded {orders.Count} orders.", logFilePath);
-                var filteredOrders = OrdersFiltration(orders, district, firstDeliveryTime);
-                Logging($"Found {filteredOrders.Count} orders for {district}.", logFilePath);
+                Log($"Loaded {orders.Count} orders.", logFilePath);
+                var filteredOrders = FilterOrders(orders, district, firstDeliveryTime);
+                Log($"Found {filteredOrders.Count} orders for {district}.", logFilePath);
 
                 Console.WriteLine();
                 try
@@ -46,25 +47,25 @@ namespace TestTaskEM
                     if (filteredOrders.Count > 0)
                     { 
                         File.Delete($"test_{district}.txt");
-                        Logging("An old one result file are deleted.", logFilePath);
+                        Log("An old one result file is deleted.", logFilePath);
                         foreach (var line in filteredOrders)
                         {
-                            Console.WriteLine($"ID: {line.OrderID}\tWeight: {line.Weight}\tDistrict: {line.CityDistrict}\tTime: {line.DeliveryTime}\n");
-                            File.AppendAllText($"test_{district}.txt", $"ID: {line.OrderID}\tWeight: {line.Weight}\tDistrict: {line.CityDistrict}\tTime: {line.DeliveryTime}\n");
+                            Console.WriteLine($"ID: {line.OrderID}\tWeight: {line.Weight}\tDistrict: {line.CityDistrict}\tTime: {line.DeliveryTime.ToString("yyyy-MM-dd HH:mm:ss")}\n");
+                            File.AppendAllText($"test_{district}.txt", $"ID: {line.OrderID}\tWeight: {line.Weight}\tDistrict: {line.CityDistrict}\tTime: {line.DeliveryTime.ToString("yyyy-MM-dd HH:mm:ss")}\n");
                         }
-                        Logging("Created the result file and log file in the project folder.", logFilePath);
+                        Log("Created the result file and log file in the project folder.", logFilePath);
                         Console.WriteLine("Created the result file and log file in the project folder.\n");
                     }
                     else Console.WriteLine("There is no data to output.");
                 }
-                catch (Exception ex) { 
-                    Logging("An error occurred when creating the result or log file.", logFilePath);
+                catch (Exception) { 
+                    Log("An error occurred when creating the result or log file.", logFilePath);
                 }
             }
             else
             { 
                 Console.WriteLine("Add the data file (test.txt) to the project folder.");
-                Logging($"Missing data file.", logFilePath);
+                Log($"Missing data file.", logFilePath);
             }
             Console.ReadKey();
         }
@@ -95,14 +96,14 @@ namespace TestTaskEM
         }
 
 
-        public static List<Orders> OrdersFiltration(List<Orders> orders, string district, DateTime firstDeliveryTime)
+        public static List<Orders> FilterOrders(List<Orders> orders, string district, DateTime firstDeliveryTime)
         {
             return orders.Where(o => o.CityDistrict == district && (firstDeliveryTime.AddMinutes(30) >= o.DeliveryTime && o.DeliveryTime >= firstDeliveryTime)).ToList();
         }
 
-        public static void Logging(string message, string logFilePath)
+        public static void Log(string message, string logFilePath)
         {
-            File.AppendAllText(logFilePath, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}: {message}\n");
+            File.AppendAllText(logFilePath, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: {message}\n");
         }
     }
 }
